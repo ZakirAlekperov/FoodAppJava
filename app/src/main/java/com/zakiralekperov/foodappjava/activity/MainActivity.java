e4f162f04f9d092dc4dc29d6c9ca80e3af4d0069
ZakirAlekperov <zakiralekperov1998@gmail.com>
2022-08-02 20:33:10 +0300

    added IntroActivity and added textFields, imageview and editText in MainActivity

--- app/src/main/java/com/zakiralekperov/foodappjava/activity/MainActivity.java
+++ app/src/main/java/com/zakiralekperov/foodappjava/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.zakiralekperov.foodappjava;
+package com.zakiralekperov.foodappjava.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.zakiralekperov.foodappjava.R;
 
 public class MainActivity extends AppCompatActivity {
 
